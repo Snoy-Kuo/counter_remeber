b9e61cf5a37c926ce30b141ea14a3aac6d2db435
Snoy Kuo <snoy.kuo@twprotech.com>
2021-09-17 12:07:39 +0800

    add counter feature by remember.

--- app/src/main/java/com/snoy/counterremember/counter/CounterScreen.kt
+++ app/src/main/java/com/snoy/counterremember/counter/CounterScreen.kt
@@ -1,18 +1,22 @@
 package com.snoy.counterremember.counter
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.snoy.counterremember.ui.theme.CounterRememberTheme
 
+//ref= https://developer.android.com/codelabs/jetpack-compose-state#5
+
 @Composable
 fun CounterScreen() {
+    var counterValue by remember { mutableStateOf(0) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -28,7 +32,10 @@ fun CounterScreen() {
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {}
+                onClick = {
+                    Log.d("RDTest", "FAB Click")
+                    counterValue++
+                }
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
@@ -44,12 +51,17 @@ fun CounterScreen() {
             Text(
                 text = "You have clicked the button this many times:",
             )
-            Text(
-                text = "0",
-                style = MaterialTheme.typography.h4,
-            )
+            CounterText(counterValue)
         }
     }
+}
+
+@Composable
+fun CounterText(counterValue: Int) {
+    Text(
+        text = "$counterValue",
+        style = MaterialTheme.typography.h4,
+    )
 }
 
 @Preview(showBackground = true)
