aceece5e89e2ada05fede45df0e3c6fe4b4d2d1b
Snoy Kuo <snoy.kuo@twprotech.com>
2021-09-17 12:27:13 +0800

    use rememberSaveable instead of remember. for letting state survive after Activity recreating (eg. rotate screen, change day/night mode).

--- app/src/main/java/com/snoy/counterremember/counter/CounterScreen.kt
+++ app/src/main/java/com/snoy/counterremember/counter/CounterScreen.kt
@@ -1,22 +1,26 @@
 package com.snoy.counterremember.counter
 
-import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.snoy.counterremember.ui.theme.CounterRememberTheme
 
-//ref= https://developer.android.com/codelabs/jetpack-compose-state#5
+//ref 1 = https://developer.android.com/codelabs/jetpack-compose-state#5
+//ref 2 = https://developer.android.com/reference/kotlin/androidx/compose/runtime/saveable/package-summary#rememberSaveable(kotlin.Array,androidx.compose.runtime.saveable.Saver,kotlin.String,kotlin.Function0)
 
 @Composable
 fun CounterScreen() {
-    var counterValue by remember { mutableStateOf(0) }
+    var counterValue by rememberSaveable { mutableStateOf(0) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -33,7 +37,6 @@ fun CounterScreen() {
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    Log.d("RDTest", "FAB Click")
                     counterValue++
                 }
             ) {
